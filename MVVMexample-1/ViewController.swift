8e4f94d53f47cc6daeb9ea4e93b10c4efe766bfe
altintopa <83283552+altintopa@users.noreply.github.com>
2022-05-16 14:26:53 +0300

    First one

---
+++ MVVMexample-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMexample-1
+//
+//  Created by Ahmet WOW on 16/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
